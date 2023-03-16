52cf09a6582d6a1f593a34e7520305e35367b728
andreatreacy <71875337+andreatreacy@users.noreply.github.com>
2023-03-16 19:18:15 +0000

    Assign 1 p2

--- Assignment1/collect_reports.c
+++ Assignment1/collect_reports.c
@@ -4,53 +4,38 @@
 #include <sys/wait.h>
 #include <syslog.h>
 
-#define SHARE_DIR "shareDirectory"
-#define DASHBOARD_DIR "dashboardDirectory"
 
 void collect_reports(void) {
     //printf("collect reports functionality could go here.. fork/cp will probably be used");
-    DIR *dir;
-    struct dirent *entry;
-    char src_path[PATH_MAX], dest_path[PATH_MAX];
-    struct stat statbuf;
-    
-    // Open source directory
-    if ((dir = opendir(SHARE_DIR)) == NULL) {
-        perror("opendir");
-        return -1;
+    pid_t pid;
+    int status;
+
+    pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        exit(1);
     }
-    
-    // Loop through source directory entries
-    while ((entry = readdir(dir)) != NULL) {
-        // Ignore special entries . and ..
-        if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
-            continue;
+    else if (pid == 0) { // child process
+        //char *args[] = {"sh", "-c", "mv shareDirectory/* dashboardDirectory/", NULL};
+        //char *args[] = {"mv", "shareDirectory/*", "dashboardDirectory/", NULL};
+        char *args[] = {"find", "shareDirectory", "-mindepth", "1", "-exec", "mv", "-t", "dashboardDirectory", "{}", "+", NULL};
+        execvp("find", args);
+        perror("execvp");
+        exit(1);
+    }
+    else { // parent process
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            printf("All contents of 'shareDirectory' moved to 'dashboardDirectory'.\n");
         }
-        
-        // Construct source and destination paths
-        snprintf(src_path, sizeof(src_path), "%s/%s", src_dir, entry->d_name);
-        snprintf(dest_path, sizeof(dest_path), "%s/%s", dest_dir, entry->d_name);
-        
-        // Check if entry is a directory
-        if (lstat(src_path, &statbuf) == 0 && S_ISDIR(statbuf.st_mode)) {
-            // Entry is a directory, recursively move its contents
-            if (move_files(src_path, dest_path) != 0) {
-                closedir(dir);
-                return -1;
-            }
-            rmdir(src_path);
-        } else {
-            // Entry is a file, move it to destination directory
-            if (rename(src_path, dest_path) != 0) {
-                perror("rename");
-                closedir(dir);
-                return -1;
-            }
+        else {
+            printf("Failed to move contents of 'shareDirectory' to 'dashboardDirectory'.\n");
         }
     }
-    
-    // Close source directory
-    closedir(dir);
-    
-    return 0;
+}
+
+
+int main ()
+{
+    collect_reports();
 }
